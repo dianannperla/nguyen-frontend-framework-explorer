23b728f9499217f4f859b4b52846aae9e71287fa
dianannperla <waafirmelyza@hotmail.com>
2025-09-04 18:47:57 +0100

    Updated babel-loader options to include preset-react for JSX support.

--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, 
+        test: /(\.js|\.jsx)$/, 
         exclude: /node_modules/, 
         use: {
           loader: 'babel-loader',
-          options: { presets: ['@babel/preset-env'] }
+          options: { presets: ['@babel/preset-env', '@babel/preset-react'] }
         }
       }
     ]
